02f257ef394cabdbfc5f64e978299ed5614fefea
Yelaman Fazyl <fazil.el2003@outlook.com>
2022-08-04 17:02:44 +0500

    fixed db file

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php 
-require "/libs/rb.php";
-$password = "";
-$db = "late_rec";
+require "libs/rb.php";
+$password = "root";
+$db = "nisrating";
 $username = "root";
 R::setup("mysql:host=127.0.0.1;dbname=$db", $username, $password);
 $isConnected = R::testConnection();
